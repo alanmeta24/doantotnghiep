d434a3ec5b3f497ceeb3f740bbf5f1e220a2a4e6
vantandev <buivantan2508@gmail.com>
2023-06-24 14:59:25 +0700

    Complete bell notify

--- client/src/components/NotifyModal.js
+++ client/src/components/NotifyModal.js
@@ -55,9 +55,7 @@ const NotifyModal = () => {
       </div>
       <hr className="mt-0" />
 
-      {notify.data.length === 0 && (
-        <img src={NoNotice} alt="NoNotice" className="w-100" />
-      )}
+      {notify.data.length === 0 && <span> Chưa có thông báo mới</span>}
 
       <div style={{ maxHeight: 'calc(100vh - 200px)', overflow: 'auto' }}>
         {notify.data.map((msg, index) => (
